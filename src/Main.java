607a734bee9c1fb38b6b1a89d0c7ebac8de2c792
Liubov B <l.boukreeva.qa@gmail.com>
2023-03-09 23:22:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 2200;
+        int mile = 20;
+        int bonus = ticketPrice / mile;
+        System.out.println(bonus);
+
+    }
+}
